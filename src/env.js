a5bb3771597b2dfbe99288ec70bd84cec7e6f1e9
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-07-09 15:19:34 +0530

    init

--- src/env.js
+++ src/env.js
@@ -12,6 +12,7 @@ export const env = createEnv({
         ? z.string()
         : z.string().optional(),
     DATABASE_URL: z.string().url(),
+    DIRECT_URL:z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -34,6 +35,7 @@ export const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    DIRECT_URL:process.env.DIRECT_URL
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
